b688152be67701ecd1400171a45e6a5aa7d84dd1
mgrouch <mgrushinskiy@gmail.com>
2021-11-29 03:06:24 -0500

    initial patch1

--- install-scripts-patch1/8-apps/16-winlink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# See: https://getpat.io/
-
-if [ $LMARCH == 'armhf' ]; then
-  wget https://github.com/la5nta/pat/releases/download/v0.12.0/pat_0.12.0_linux_armhf.deb
-  dpkg -i pat_0.12.0_linux_armhf.deb && rm pat_0.12.0_linux_armhf.deb
-fi
