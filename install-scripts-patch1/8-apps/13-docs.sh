58dac317fd63f6703a3c1a63f9cf3a98afacc84e
mgrouch <mgrushinskiy@gmail.com>
2021-11-29 02:54:57 -0500

    initial patch1

---
+++ install-scripts-patch1/8-apps/13-docs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+install -d -o 1000 -g 1000 /home/user/Documents/
+
+wget -q -O - https://bareboat-necessities.github.io/my-bareboat/bareboat-os.pdf > /home/user/Documents/bareboat-os.pdf
