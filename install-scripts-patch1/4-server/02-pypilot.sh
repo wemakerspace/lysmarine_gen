157fb5c1f041cb8fa722240672f586ec6d51d90b
mgrouch <mgrushinskiy@gmail.com>
2021-12-01 11:16:13 -0500

    fix for import

--- install-scripts-patch1/4-server/02-pypilot.sh
+++ install-scripts-patch1/4-server/02-pypilot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
 install -v -o pypilot -g pypilot -m 0644 $FILE_FOLDER/blacklist_serial_ports "/home/pypilot/.pypilot/"
+
+cp $FILE_FOLDER/wind.py "$(find /usr/local/lib -name wind.py)"
